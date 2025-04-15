1758c0d69762d120718cff133f22e202f0bbab89
[AdrianL] <[adrianlee061123@gmail.com]>
2025-04-15 14:09:27 +0800

    Setup Project

--- app/src/main/java/com/dydrian/quizCaseStudy/ui/HomeFragment.kt
+++ app/src/main/java/com/dydrian/quizCaseStudy/ui/HomeFragment.kt
@@ -5,12 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.dydrian.quizCaseStudy.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
