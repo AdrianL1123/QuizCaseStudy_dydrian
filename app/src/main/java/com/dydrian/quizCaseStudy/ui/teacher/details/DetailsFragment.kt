0f54057fdfbb43a4bbf0917dc890ddb7b6276012
DylanNg2412 <chunxiandylan23n@gmail.com>
2025-04-29 15:37:53 +0800

    removed logs from details page

--- app/src/main/java/com/dydrian/quizCaseStudy/ui/teacher/details/DetailsFragment.kt
+++ app/src/main/java/com/dydrian/quizCaseStudy/ui/teacher/details/DetailsFragment.kt
@@ -4,7 +4,6 @@ import android.app.AlertDialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -51,7 +50,6 @@ class DetailsFragment : BaseFragment() {
                         "Timer per question: ${quiz.timePerQuestion} seconds"
                     binding.tvTotalQuestions.text = "Number of questions: ${quiz.questions?.size}"
                     binding.tvQuestionsList.text = formatQuestions(quiz.questions)
-                    Log.d("debugging", "Questions: ${quiz.questions}")
                 }
             }
         }
--- app/src/main/java/com/dydrian/quizCaseStudy/ui/teacher/details/DetailsViewModel.kt
+++ app/src/main/java/com/dydrian/quizCaseStudy/ui/teacher/details/DetailsViewModel.kt
@@ -1,16 +1,13 @@
 package com.dydrian.quizCaseStudy.ui.teacher.details
 
 import android.content.Context
-import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.dydrian.quizCaseStudy.core.showToast
 import com.dydrian.quizCaseStudy.data.model.Quiz
 import com.dydrian.quizCaseStudy.data.repo.QuizRepo
 import com.dydrian.quizCaseStudy.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
